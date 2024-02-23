c4114b3064ca58867ad5133106a5a88256a77867
Ankit Tripathi <95411215+ANKITTRIPATHI99@users.noreply.github.com>
2024-02-23 18:41:43 +0000

    linear search

---
+++ Searching_and_Sorting/LinearSearch.java
@@ -0,0 +1,40 @@
+package Searching_and_Sorting;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+          Scanner sc=new Scanner(System.in);
+
+          System.out.println("Enter the no of elements in array");
+
+          int n = sc.nextInt();
+
+          System.out.println("Enter the elemets of  array");
+
+          int arr[] = new int[n];
+
+          for(int i=0; i<n; i++){
+              arr[i]=sc.nextInt();
+          }
+
+          int k = sc.nextInt()
+
+          
+
+          System.out
+
+    }
+
+    p
+        for(int i=0; i<n; i++){
+
+            if(i==k){
+                System.out.println(k);
+            }
+        }
+    }
+    
+}
